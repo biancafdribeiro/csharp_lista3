3ab62bb9c08aa2ab9fb583f98797589eb83f5e59
Bianca Ribeiro <132418587+biancafdribeiro@users.noreply.github.com>
2023-09-03 17:55:17 -0300

    Exercício 8

---
+++ Lista3_Ex8/Produto.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Crie uma classe Produto com os atributos privados nome (string) e preco (double). Implemente
+os métodos GetNome() e SetNome(string name) para acessar e modificar o nome do produto, e
+os métodos GetPreco() e SetPreco(double price) para acessar e modificar o preço do produto.*/
+
+/*Expanda o exemplo da classe Produto do primeiro conjunto de exercícios, mas desta vez,
+implemente a propriedade Nome usando get e set para acessar e modificar o nome do
+produto*/
+
+namespace Lista3_Ex8
+{
+
+    internal class Produto
+    {
+        private string Nome
+        {
+            get
+            {
+                return nome;
+            }
+
+            set
+            {
+                Nome = value;
+            }
+        }
+
+        public string nome;
+        private double preco { get; set; }
+        private string NomeProduto { get; set; }
+        private double PrecoProduto { get; set; }
+
+        /*public string GetNome()
+        {
+            return nome;
+        }
+
+        public void SetNome(string NovoNome) //precisa ser void porque não vai retornar nada
+        {
+            nome = NovoNome; 
+        }*/
+
+        public double GetPreco()
+        {
+            return preco;
+        }
+
+        public void SetPreco(double NovoPreco)
+        {
+            preco = NovoPreco;
+        }
+    }
+}
---
+++ Lista3_Ex8/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Crie uma classe Produto com os atributos privados nome (string) e preco (double). Implemente
+os métodos GetNome() e SetNome(string name) para acessar e modificar o nome do produto, e
+os métodos GetPreco() e SetPreco(double price) para acessar e modificar o preço do produto.*/
+
+/*Expanda o exemplo da classe Produto do primeiro conjunto de exercícios, mas desta vez,
+implemente a propriedade Nome usando get e set para acessar e modificar o nome do
+produto*/
+
+namespace Lista3_Ex8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto = new Produto(); // objeto
+
+            // nome: modifiquei para usar a propriedade Nome
+
+            produto.nome = ("Bola");
+            string nomeproduto = produto.nome;
+            Console.WriteLine("Nome do Produto: " + nomeproduto);
+
+            // produto
+
+            produto.SetPreco(10.0); // double não precisa de aspas
+
+            double PrecoProduto = produto.GetPreco();
+
+            Console.WriteLine("Preço do Produto: " + PrecoProduto);
+
+            Console.ReadKey();
+        }
+    }
+}
