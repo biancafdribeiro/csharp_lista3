f8a02d34c505194a891821632042474a11bc9730
Bianca Ribeiro <132418587+biancafdribeiro@users.noreply.github.com>
2023-09-03 16:04:46 -0300

    Exercício 5

---
+++ Lista3_Ex5/Empregado.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+    /* Crie uma classe Empregado com os atributos privados primeiroNome (string) e ultimoNome
+    (string). Implemente os métodos GetNomeCompleto() e SetNomeCompleto(string
+    primeiroNome, string ultimoNome) para acessar e modificar o nome completo do funcionário.*/
+
+namespace Lista3_Ex5
+{
+    internal class Empregado
+    {
+        private string primeiroNome { get; set; }
+        private string ultimoNome { get; set; } 
+        private string nomeCompleto { get; set; }
+
+        public string GetNomeCompleto() { return nomeCompleto; }
+        public void SetNomeCompleto(string primeiroNome, string ultimoNome) { nomeCompleto = primeiroNome + ultimoNome; }
+    }
+}
---
+++ Lista3_Ex5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    /* Crie uma classe Empregado com os atributos privados primeiroNome (string) e ultimoNome
+    (string). Implemente os métodos GetNomeCompleto() e SetNomeCompleto(string
+    primeiroNome, string ultimoNome) para acessar e modificar o nome completo do funcionário.*/
+
+namespace Lista3_Ex5
+{
+internal class Program
+{
+    static void Main(string[] args)
+    {
+            Empregado objetoempregado = new Empregado();
+            objetoempregado.SetNomeCompleto("Bianca ", "Ribeiro");
+            string nomeCompleto = objetoempregado.GetNomeCompleto();
+            Console.WriteLine("O nome completo do(a) empregado(a) é: " + nomeCompleto);
+            Console.ReadKey();
+    }
+}
+}
