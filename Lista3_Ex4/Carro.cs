38e09b65b28ba3573bba9861f1d353739f1c7416
Bianca Ribeiro <132418587+biancafdribeiro@users.noreply.github.com>
2023-09-03 15:51:45 -0300

    Exercício 4

---
+++ Lista3_Ex4/Carro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    /*4. Crie uma classe Carro com os atributos privados marca (string) e ano (int). Implemente os
+    métodos GetMarca() e SetMarca(string marca) para acessar e modificar a marca do carro, e os
+    métodos GetAno() e SetAno(int ano) para acessar e modificar o ano do carro.*/
+
+namespace Lista3_Ex4
+{
+    internal class Carro
+    {
+        private string marca { get; set; }
+        private int ano { get; set; }   
+
+        public string GetMarca() { return marca; }
+        public void SetMarca(string marca) { this.marca = marca; } //atribuindo o valor do parâmetro marca
+                                                                   //ao campo marca da classe
+        public int GetAno() { return ano; }
+        public void SetAno(int ano) { this.ano = ano;}
+    }
+}
---
+++ Lista3_Ex4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    /*4. Crie uma classe Carro com os atributos privados marca (string) e ano (int). Implemente os
+    métodos GetMarca() e SetMarca(string marca) para acessar e modificar a marca do carro, e os
+    métodos GetAno() e SetAno(int ano) para acessar e modificar o ano do carro.*/
+
+namespace Lista3_Ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro objetocarro = new Carro();
+
+            objetocarro.SetMarca("Volkswagen");
+            string marca = objetocarro.GetMarca();
+            Console.WriteLine("A marca do carro é: " + marca);
+
+            objetocarro.SetAno(2012);
+            int ano = objetocarro.GetAno();
+            Console.WriteLine("O ano do carro é: " + ano);
+
+            Console.ReadKey();
+        }
+    }
+}
