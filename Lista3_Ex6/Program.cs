265a5a76e57870cbb7303df4af0fc0cf267b74d5
Bianca Ribeiro <132418587+biancafdribeiro@users.noreply.github.com>
2023-09-03 17:01:46 -0300

    Exercício 6

---
+++ Lista3_Ex6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Crie uma classe Temperatura com um atributo privado celsius (double). Implemente uma
+propriedade Celsius usando get e set para acessar e modificar a temperatura em graus Celsius.
+Adicione um método ConverteParaFahrenheit() que retorna a temperatura em Fahrenheit.*/
+
+namespace Lista3_Ex6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Temperatura objetotemperatura = new Temperatura();
+            double fahrenheit = objetotemperatura.ConverteParaFahrenheit(20.0); //lendo e atribuindo ao mesmo tempo
+            Console.WriteLine("A temperatura em Fahrenheit é: " + fahrenheit);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lista3_Ex6/Temperatura.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Crie uma classe Temperatura com um atributo privado celsius (double). Implemente uma
+propriedade Celsius usando get e set para acessar e modificar a temperatura em graus Celsius.
+Adicione um método ConverteParaFahrenheit() que retorna a temperatura em Fahrenheit.*/
+
+namespace Lista3_Ex6
+{
+    internal class Temperatura
+    {
+        private double celsius;
+        public double Celsius
+        { get { return celsius; } 
+          set { celsius = value;} }
+
+        public double ConverteParaFahrenheit(double valorCelsius)
+        {
+            double fahrenheit = valorCelsius * 1.8 + 32.0;
+            return fahrenheit;
+        }
+    }
+}
