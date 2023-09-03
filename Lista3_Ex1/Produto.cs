e97ccff74e2c8b91994f87264f24b06e6d97b299
Bianca Ribeiro <132418587+biancafdribeiro@users.noreply.github.com>
2023-09-03 04:25:32 -0300

    Exercício 1

---
+++ Lista3_Ex1/Produto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3_Ex1
+{
+    /*Crie uma classe Produto com os atributos privados nome (string) e preco (double). Implemente
+    os métodos GetNome() e SetNome(string name) para acessar e modificar o nome do produto, e
+    os métodos GetPreco() e SetPreco(double price) para acessar e modificar o preço do produto.*/
+
+    internal class Produto
+    {
+        private string nome { get; set; }
+        private double preco { get; set; }
+        private string NomeProduto { get; set; }
+        private double PrecoProduto { get; set; }
+
+        public string GetNome()
+        {
+            return nome;
+        }
+
+        public void SetNome(string NovoNome) //precisa ser void porque não vai retornar nada
+        {
+            nome = NovoNome; 
+        }
+        
+        public double GetPreco()
+        {
+            return preco;
+        }
+
+        public void SetPreco(double NovoPreco)
+        {
+            preco = NovoPreco;
+        }
+    }
+}
+ 
---
+++ Lista3_Ex1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3_Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto = new Produto(); // objeto
+
+            // nome
+
+            produto.SetNome("Bola");
+
+            string NomeProduto = produto.GetNome();
+
+            Console.WriteLine("Nome do Produto: " +  NomeProduto);
+
+
+            // produto
+
+            produto.SetPreco(10.0); // double não precisa de aspas
+
+            double PrecoProduto = produto.GetPreco();
+
+            Console.WriteLine("Preço do Produto: " + PrecoProduto);
+
+            Console.ReadKey();
+        }
+    }
+}
