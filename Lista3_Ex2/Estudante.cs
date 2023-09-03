305faa1827ecdba8e0457b8c44612495fe623b36
Bianca Ribeiro <132418587+biancafdribeiro@users.noreply.github.com>
2023-09-03 04:26:11 -0300

    Exercício 2

---
+++ Lista3_Ex2/Estudante.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    /*Crie uma classe Estudante com os atributos privados nome (string), sobrenome (string) e idade
+    (int). Implemente os métodos GetNome() e SetNome(string nome) para acessar e modificar o
+    nome do estudante, os métodos GetNome()para acessar o sobrenome do estudante e os
+    métodos GetIdade() e SetIdade(int idade) para acessar e modificar a idade do estudante. O
+    sobrenome do estudante não deve poder ser atribuído fora da classe e deve ter seu valor
+    padrão igual a “Oliveira”.*/
+
+namespace Lista3_Ex2
+{
+    internal class Estudante
+    {
+        private string nome { get; set; }
+        private string NovoNome { get; set; }
+        public string sobrenome { get; private set; } = "Oliveira";
+        public string NovoSobrenome { get; set; }
+        private int idade { get; set; }
+        public int NovaIdade { get; set; }
+
+
+        public string GetNome() { return nome; }
+        public void SetNome(string NovoNome) { nome = NovoNome; }
+
+        public string GetSobrenome() {  return sobrenome; }
+        public void SetSobrenome(string NovoSobrenome) { sobrenome = NovoSobrenome; }
+
+        public int GetIdade() {  return idade; }
+        public void SetIdade(int NovaIdade) { idade = NovaIdade; }
+
+
+    }
+}
---
+++ Lista3_Ex2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    /*Crie uma classe Estudante com os atributos privados nome (string), sobrenome (string) e idade
+    (int). Implemente os métodos GetNome() e SetNome(string nome) para acessar e modificar o
+    nome do estudante, os métodos GetNome()para acessar o sobrenome do estudante e os
+    métodos GetIdade() e SetIdade(int idade) para acessar e modificar a idade do estudante. O
+    sobrenome do estudante não deve poder ser atribuído fora da classe e deve ter seu valor
+    padrão igual a “Oliveira”.*/
+
+namespace Lista3_Ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudante estudante = new Estudante(); // objeto
+
+            estudante.SetNome("Bianca"); //método coloco direto 
+            string NovoNome = estudante.GetNome();
+            Console.WriteLine("O nome do estudante é: " + NovoNome);
+
+            string NovoSobrenome = estudante.GetSobrenome();
+            Console.WriteLine("O sobrenome do estudante é: " + NovoSobrenome);
+
+            estudante.SetIdade(20);
+            int NovaIdade = estudante.GetIdade();
+            Console.WriteLine("A idade do estudante é: " + NovaIdade);
+
+            Console.ReadKey();
+        }
+    }
+}
