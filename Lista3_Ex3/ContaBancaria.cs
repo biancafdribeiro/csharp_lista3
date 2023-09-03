93041741690bbe917e1c5ae9b3ae99e032d368c0
Bianca Ribeiro <132418587+biancafdribeiro@users.noreply.github.com>
2023-09-03 15:34:36 -0300

    Exercício 3

---
+++ Lista3_Ex3/ContaBancaria.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    /*Crie uma classe ContaBancaria com os atributos privados saldo (double) e numeroConta
+    (string). Implemente os métodos GetSaldo() e SetSaldo(double valor) para acessar e modificar o
+    saldo da conta.*/
+
+namespace Lista3_Ex3
+{
+    internal class ContaBancaria
+    {
+        private double saldo { get; set; }
+        private string numeroConta { get; set; }
+        private double valor { get; set; }
+
+
+        public double GetSaldo() { return saldo; }
+        public void SetSaldo(double valor) { saldo = valor; }
+
+    }
+}
---
+++ Lista3_Ex3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    /*Crie uma classe ContaBancaria com os atributos privados saldo (double) e numeroConta
+    (string). Implemente os métodos GetSaldo() e SetSaldo(double valor) para acessar e modificar o
+    saldo da conta.*/
+namespace Lista3_Ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria conta = new ContaBancaria();
+            conta.SetSaldo(1200.0);
+            double ContaBancaria = conta.GetSaldo();
+            Console.WriteLine("O saldo da conta é: " + ContaBancaria);
+            Console.ReadKey();
+
+        }
+    }
+}
