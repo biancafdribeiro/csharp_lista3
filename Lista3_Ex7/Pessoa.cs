43297d93dceb103683f72c50352d9d9b40a15c04
Bianca Ribeiro <132418587+biancafdribeiro@users.noreply.github.com>
2023-09-03 17:20:50 -0300

    Exercício 7

---
+++ Lista3_Ex7/Pessoa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Crie uma classe Pessoa com um atributo privado anoAniversario (int). Implemente uma
+propriedade Idade usando get para calcular e retornar a idade da pessoa com base no ano de
+nascimento*/
+
+namespace Lista3_Ex7
+{
+    internal class Pessoa
+    {
+        private int anoAniversario;
+        public int idade 
+        { get { return 2023 - anoAniversario; } 
+        set { anoAniversario = value; } }
+    }
+}
---
+++ Lista3_Ex7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Crie uma classe Pessoa com um atributo privado anoAniversario (int). Implemente uma
+propriedade Idade usando get para calcular e retornar a idade da pessoa com base no ano de
+nascimento*/ 
+
+namespace Lista3_Ex7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa = new Pessoa();
+            pessoa.idade = (1997);
+            int idadepessoa = pessoa.idade;
+
+            Console.WriteLine("A idade da pessoa é: " + idadepessoa);
+            Console.ReadKey();
+        }
+    }
+}
